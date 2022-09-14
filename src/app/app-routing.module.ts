2d10d5ed3a034d56d101857e191c5ab5b2ae6260
Gazali <gazali.ahmad@gmail.com>
2022-09-14 17:46:02 +0800

    Added navbar navigation items

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
+import { AllreviewsComponent } from "./reviews/allreviews/allreviews.component";
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { AllactorsComponent } from "./actors/allactors/allactors.component";
+import { AllmoviesComponent } from "./movies/allmovies/allmovies.component";
 
 const routes: Routes = [
   { path: "", component: LoginComponent },
   { path: "login", component: LoginComponent },
   { path: "home", component: HomeComponent },
+  { path: "movies", component: AllmoviesComponent },
+  { path: "reviews", component: AllreviewsComponent },
   { path: "**", component: LoginComponent },
 ];
 
